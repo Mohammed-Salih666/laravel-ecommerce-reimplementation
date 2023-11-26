7c14c951ac4cb45017a695e1880223d095859e64
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-11-26 15:36:22 +0300

    define the prepare() method that returns the right array of fillable data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function prepare(Request $request, $fillables) 
+    {
+        $data = array();
+        foreach($fillables as $fillable) {
+            if($request->has($fillable)) {
+                $data[$fillable] = $request->get($fillable); 
+            }
+        }
+
+        return $data;
+    }
 }
