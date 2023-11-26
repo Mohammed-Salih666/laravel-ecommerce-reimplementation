5db0376706c5b6e2c9946b14bd3be0ca3ffb5119
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-11-26 15:35:35 +0300

    define user fillables

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin', 
+        'is_active', 
     ];
 
     /**
@@ -31,6 +33,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'is_admin'
     ];
 
     /**
