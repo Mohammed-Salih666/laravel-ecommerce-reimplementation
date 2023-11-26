4348753aa708d848023ac2e9a7e389f15e815577
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-11-26 16:49:35 +0300

    Add a hasMany relationship with the Address Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,4 +47,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function address() : HasMany {
+        return $this->hasMany(Address::class); 
+    }
 }
