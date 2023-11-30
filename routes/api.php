3157caea17d884363767a6623ec21c174d95151d
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-11-30 15:52:49 +0300

    define the routes for categories and products

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +24,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('/users', UserController::class); 
+
 Route::resource('/users/{user}/addresses', AddressController::class);
+
 Route::get('/users/{user}/cart', [CartController::class, 'index']);
 Route::post('/users/{user}/cart/add-product', [CartController::class, 'add']);
 Route::delete('/users/{user}/cart', [CartController::class, 'remove']);
+
+Route::resource('/products', ProductController::class); 
+
+Route::resource('/categories', CategoryController::class);
+Route::get('/categories/{category}/products', [CategoryController::class, 'getProducts']);
