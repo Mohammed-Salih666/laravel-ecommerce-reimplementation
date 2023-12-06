8fbf7e2b42ed0659fcc3ecb4a95805168012524a
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-12-06 13:09:10 +0300

    update the routes for the application

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
-use App\Http\Controllers\AddressController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,19 +23,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//Public: 
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::resource('/products', ProductController::class)->only(['index', 'show']); 
+
+Route::resource('/categories', CategoryController::class)->only(['index', 'show']);
+Route::get('/categories/{category}/products', [CategoryController::class, 'getProducts']);
+
+//Private: 
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::resource('/users/{user}/addresses', AddressController::class);
+
+    Route::get('/users/{user}/cart', [CartController::class, 'index']);
+    Route::post('/users/{user}/cart/add-product', [CartController::class, 'add']);
+    Route::delete('/users/{user}/cart', [CartController::class, 'remove']);
+
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::resource('/users', UserController::class); 
-
-Route::resource('/users/{user}/addresses', AddressController::class);
-
-Route::get('/users/{user}/cart', [CartController::class, 'index']);
-Route::post('/users/{user}/cart/add-product', [CartController::class, 'add']);
-Route::delete('/users/{user}/cart', [CartController::class, 'remove']);
-
-Route::resource('/products', ProductController::class); 
-
-Route::resource('/categories', CategoryController::class);
-Route::get('/categories/{category}/products', [CategoryController::class, 'getProducts']);
+//Admin Operations
+Route::middleware('auth:sanctum', 'admin')->group(function() {
+    Route::post('/authorize', [AuthController::class, 'authorizeTo']);
+    Route::post('/unauthorize', [AuthController::class, 'unauthorize']);
+    Route::resource('/users', UserController::class); 
+    Route::resource('/products', ProductController::class)->except(['index', 'show']);
+    Route::resource('/categories', CategoryController::class)->except(['index', 'show']);
+});
