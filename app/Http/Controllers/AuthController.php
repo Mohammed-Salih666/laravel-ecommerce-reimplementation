b6cfc998c71e8fa043326f39fdbd2359d69983e5
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-12-09 18:50:54 +0300

    Add the resetPassword() function to the AuthController. The logic here was directly followed the one in the documentation, as this logic is conventional and does not need re-invention

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Spatie\Permission\Models\Permission;
 
 class AuthController extends Controller
@@ -64,6 +67,31 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'logged out'
         ]);
+    }
+
+    public function resetPassword(Request $request) {
+        $request->validate([
+            'token' => 'required', 
+            'email' => 'required|email', 
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password-confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60)); 
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        ); 
+
+        return $status === Password::PASSWORD_RESET 
+                ? redirect()->route('login')->with('status', __($status))
+                : back()->withErrors(['email' => [__($status)]]);
     }
 
     //Request Signature: 
