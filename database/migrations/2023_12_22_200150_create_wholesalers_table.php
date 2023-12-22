ed0a8f2641938585ac1ffb84cde34a30bb77d761
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-12-22 23:58:23 +0300

    define the wholesalers table attributes

---
+++ database/migrations/2023_12_22_200150_create_wholesalers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wholesalers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->references('users')->on('id'); 
+            $table->string('name'); 
+            $table->string('address'); 
+            $table->boolean('is_active');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wholesalers');
+    }
+};
