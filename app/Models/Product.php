abb7c82cfa0f8a2130a3e418bf68e75e7ede8d45
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-12-22 23:56:53 +0300

    define a belongs-to-many relationship with warehouse

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
@@ -27,6 +28,11 @@ class Product extends Model
 
     public function images() {
         return $this->hasMany(ProductImage::class); 
+    }
+
+    public function warehouses() : BelongsToMany
+    {
+        return $this->belongsToMany(Warehouse::class, 'warehouse_product', 'product_id', 'warehouse_id')->withPivot(['quantity', 'created_at', 'updated_at', 'is_active']); 
     }
     // public function category() : HasOne {
     //     return $this->hasOne(Category::class); 
