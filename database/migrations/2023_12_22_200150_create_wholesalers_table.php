5cf22405b0e5951982e7a21bb9b7ec5f937a5a56
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-12-23 19:44:38 +0300

    add the email column to the wholesalers table

--- database/migrations/2023_12_22_200150_create_wholesalers_table.php
+++ database/migrations/2023_12_22_200150_create_wholesalers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('owner_id')->references('users')->on('id'); 
             $table->string('name'); 
+            $table->string('email'); 
             $table->string('address'); 
             $table->boolean('is_active');
             $table->timestamp('deleted_at')->nullable();
